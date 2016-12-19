fbd90896f3c68df79a4a99ad5cb746f402f24c1c
Matthew Ramuta <matthew.g.ramuta@gmail.com>
2016-12-18 18:07:24 -0600

    add messages table view and controller

--- swift_app/SEEK_R/MessagesTableViewController.swift
+++ swift_app/SEEK_R/MessagesTableViewController.swift
@@ -9,8 +9,10 @@
 import UIKit
 
 class MessagesTableViewController: UITableViewController {
-
+    var convoNames = [String: String]()
+    
     override func viewDidLoad() {
+        requestConversations("http://localhost:3000/messages")
         super.viewDidLoad()
 
         // Uncomment the following line to preserve selection between presentations
@@ -27,20 +29,70 @@ class MessagesTableViewController: UITableViewController {
 
     // MARK: - Table view data source
     override func numberOfSections(in tableView: UITableView) -> Int {
+        //print(convoNames.count)
         return 1
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 9
+        return convoNames.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "LabelCell", for: indexPath)
         
-        cell.textLabel?.text = "Section \(indexPath.section) Row \(indexPath.row)"
+        cell.textLabel?.text = " Person \(convoNames[String(indexPath.row)]!)"
         
         return cell
     }
+    
+    func requestConversations(_ url: String){
+        var request = URLRequest(url: URL(string: url)!)
+        request.httpMethod = "GET"
+        let session = URLSession.shared
+        //let postString = "name=\(usernameField.text!)&password=\(password.text!)"
+        //let postString = "user%5Busername%5D=\(usernameField.text!)&user%5Bpassword%5D=\(password.text!)"
+        let task = session.dataTask(with: request as URLRequest, completionHandler: {
+            (
+            data, response, error) in
+            guard let _:Data = data, let _:URLResponse = response  , error == nil else {
+                return
+            }
+            let json: Any?
+            do
+            {
+                json = try JSONSerialization.jsonObject(with: data!, options: [])
+                print(json ?? "there is no json")
+            }
+            catch
+            {
+                return
+            }
+            guard let server_response = json as? NSDictionary else
+            {
+                return
+            }
+            
+            
+            if let data_block = server_response["data"] as? NSDictionary
+            {
+                if let conversations = data_block["conversations"] as? NSDictionary
+                {
+                    self.convoNames = conversations as! [String : String]
+                    print(self.convoNames)
+                    self.tableView.reloadData()
+                }
+
+            }
+            
+            
+            
+        })
+        
+        task.resume()
+        
+
+    }
+    
     /*
     override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
         return "Section \(section)"
